ad283143d2f4dffc2cced4770217089b084f09ee
Red 2 <Red 2@10.220.240.163>
2016-01-19 16:39:23 -0800

    Intake angle PID added. Might take off the D. Not sure if there are bugs or not.

--- src/org/usfirst/frc/team668/robot/Intake.java
+++ src/org/usfirst/frc/team668/robot/Intake.java
@@ -2,25 +2,86 @@ package org.usfirst.frc.team668.robot;
 
 public class Intake {
 	
+	public static double Kp = 0;
+	public static double Ki = 0;
+	public static double Kd = 0;
+	public static long lastTime = System.currentTimeMillis();
+	public static double speed;
+	public static int error = 0;
+	public static int lastError = 0;
+	public static double i = 0;
+	public static double d = 0;
+	public static double P, I, D;
+	
+	public static boolean movePID (int ref){
+		
+		error = ref = Robot.intakeEncoder.get();
+		
+		long currentTime = System.currentTimeMillis();
+		
+		i = i + (currentTime - lastTime)*(error);
+		
+		d = (error - lastError)/(currentTime- lastTime);
+		
+		P = Kp * error;
+		I = Ki * i;
+		D = Kd * d;
+		speed = P + I + D;
+		
+		
+		
+		if(Math.abs(speed) > 1) {
+			if(speed > 1){
+				speed = 1;
+			}
+			else{
+				speed = -1;
+			}
+		}
+		
+		Robot.canTalonIntakeAngle.set(speed);
+		
+		lastError = error;
+		currentTime = lastTime;
+			
+		if (!Robot.intakeLimitTop.get() || !Robot.intakeLimitBot.get()){
+			Robot.canTalonIntakeAngle.set(0);
+			return true;
+		}
+		
+		
+		
+		if (Math.abs(error) <= 4){
+			Robot.canTalonIntakeAngle.set(0);
+			return true;
+		}
+		else{
+			return false;
+		}
+				
+	
+	}
+	
+	
 	public static void spin(double speed){
 		
 		Robot.canTalonIntakeOne.set(speed);
-		Robot.canTalonintakeTwo.set(speed);
+		Robot.canTalonIntakeTwo.set(speed);
 		
 	}
 	
 	public static void spit(double speed){
 		
 		Robot.canTalonIntakeOne.set(-speed);
-		Robot.canTalonintakeTwo.set(-speed);
+		Robot.canTalonIntakeTwo.set(-speed);
 		
 	}
 	
 	public static void stop(){
 		
 		Robot.canTalonIntakeOne.set(0.0);
-		Robot.canTalonintakeTwo.set(0.0);
+		Robot.canTalonIntakeTwo.set(0.0);
 		
 	}
-
+	
 }
