d9c37c864e0d947681a9edd294eba5d248eb6cd5
Red 1 <Red 1@10.220.249.0>
2016-01-22 20:03:51 -0800

    pre rebase commit

--- src/org/usfirst/frc/team668/robot/Robot.java
+++ src/org/usfirst/frc/team668/robot/Robot.java
@@ -6,19 +6,28 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	
    public static Joystick joyWheel, joyThrottle, joyOp;
-   public static CANTalon canTalonFlyWheel, canTalonTrigger, canTalonIntakeOne, 
-   							canTalonIntakeTwo, canTalonFrontLeft, canTalonFrontRight, canTalonRearLeft, canTalonRearRight;
+   public static CANTalon canTalonFlyWheel, canTalonTrigger, canTalonIntake, 
+   							 canTalonFrontLeft, canTalonFrontRight, canTalonRearLeft, canTalonRearRight,
+   							canTalonIntakeAngle, canTalonShooterAngle;
    public static RobotDrive robotDrive;
-   	
-   
+   public static Encoder encoderLeft, encoderRight, flywheelEncoder, intakeEncoder, angleEncoder;
+   public static DigitalInput shooterLimitTop, shooterLimitBot, intakeLimitTop, intakeLimitBot; 
+   public static CameraServer server;
    
     public void robotInit() {
+    	
+    	server = CameraServer.getInstance();
+    	server.setQuality(50);
+    	server.startAutomaticCapture("cam1");
         
     	 joyWheel = new Joystick(0);
          joyThrottle = new Joystick(1);
@@ -28,31 +37,33 @@ public class Robot extends IterativeRobot {
          canTalonFlyWheel = new CANTalon(0);
          canTalonTrigger = new CANTalon(1);
          
-         canTalonIntakeOne = new CANTalon(2);
-         canTalonIntakeTwo = new CANTalon(3);
+         canTalonIntake = new CANTalon(2);
          
-         canTalonFrontLeft = new CANTalon(4);
-         canTalonFrontRight = new CANTalon(5);
-         canTalonRearLeft = new CANTalon(6);
-         canTalonRearRight = new CANTalon(7);
+         canTalonFrontLeft = new CANTalon(3);
+         canTalonFrontRight = new CANTalon(4);
+         canTalonRearLeft = new CANTalon(5);
+         canTalonRearRight = new CANTalon(6);
 
-         canTalonFlyWheel = new CANTalon(2);
-         canTalonTrigger = new CANTalon(3);
-         
-         canTalonIntakeOne = new CANTalon(4);
-         canTalonIntakeTwo = new CANTalon(5);
-         
-         canTalonFrontLeft = new CANTalon(6);
-         canTalonFrontRight = new CANTalon(7);
-         canTalonRearLeft = new CANTalon(8);
-         canTalonRearRight = new CANTalon(9);
+         canTalonIntakeAngle = new CANTalon(7);
+         canTalonShooterAngle = new CANTalon(8);
          
          robotDrive = new RobotDrive(canTalonFrontLeft, canTalonRearLeft, canTalonFrontRight, canTalonRearRight);
          robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
      	 robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
          robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
      	 robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
-         
+     	 
+     	 encoderRight = new Encoder(0,1);
+     	 encoderLeft = new Encoder(2,3);
+     	 flywheelEncoder = new Encoder(4,5);
+     	 intakeEncoder = new Encoder(6,7);
+     	 angleEncoder = new Encoder(8,9);
+     	 
+     	 shooterLimitTop = new DigitalInput(0);
+     	 shooterLimitBot = new DigitalInput(1);
+     	 intakeLimitTop = new DigitalInput(2);
+     	 intakeLimitBot = new DigitalInput(3);
+     	 
     }
     
     public void autonomousInit() {
@@ -71,6 +82,9 @@ public class Robot extends IterativeRobot {
     	boolean isIntaking = joyOp.getRawButton(3);
     	boolean isReverse = joyOp.getRawButton(5);
     	boolean isFire = joyOp.getRawButton(1);
+    	boolean isIntakeLower = joyOp.getRawButton(8);
+    	boolean isIntakeRise = joyOp.getRawButton(7);
+    	
     	
     	//Drive Code
     	if (isMinimize){
@@ -80,29 +94,40 @@ public class Robot extends IterativeRobot {
     		robotDrive.arcadeDrive(joyThrottle.getY(), joyWheel.getX());
     	}
     	
+    	if (isIntakeLower){
+    		Intake.movePID(0);
+    	}
+    	if (isIntakeRise){
+    		Intake.movePID(0);
+    	}
+    	
+    	
     	//intake
     	if (isIntaking){
     		Intake.spin(.8);
     	}
-    	else{
-    		Intake.stop();
+    		else{
+    			Intake.stop();
     	}
     	
     	//reverse intake
     	if (isReverse){
     		Intake.spit(.8);
     	}
-    	else{
-    		Intake.stop();
+    		else{
+    			Intake.stop();
     	}
     	
     	//Firing
     	if (isFire){
-    		Shooter.fire(1.0); //TODO: change to controller
+    		Shooter.fire(0); //TODO: change to controller
     	}
-    	else{
-    		Shooter.stop();
+    		else{
+    			Shooter.stop();
     	}
+    	
+    	
+    	
     
     }
    
