171dc9a7b5a211edf1ce92607f50f532e314ea69
Red 1 <Red 1@10.220.249.0>
2016-01-31 16:27:09 -0800

    end of day 1/31/16

--- src/org/usfirst/frc/team668/robot/Robot.java
+++ src/org/usfirst/frc/team668/robot/Robot.java
@@ -21,212 +21,206 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
 public class Robot extends IterativeRobot {
-	
-   public static Joystick joyWheel, joyThrottle, joyOp;
-   public static CANTalon canTalonFlyWheel, canTalonIntake, 
-   							 canTalonFrontLeft, canTalonFrontRight, canTalonRearLeft, canTalonRearRight,
-   							canTalonIntakeAngle, canTalonShooterAngle;
-   public static RobotDrive robotDrive;
-   public static DigitalInput opticSensor1, opticSensor2;
-  // public static CameraServer server;
-   public static PrintWriter system;
-   public static DoubleSolenoid intakePiston, shiftRight, shiftLeft, shooterPiston;
-   public static Compressor compressor;
-   public boolean isClose;
-//   public static USBCamera camFront = new USBCamera("cam1");
-//   public static USBCamera camRear = new USBCamera("cam2");
-    public void robotInit() {
-    	
-//    	server = CameraServer.getInstance();
-//    	server.setQuality(50);
-//    	camFront.openCamera();
-//    	camRear.openCamera();
-       
-    	 joyWheel = new Joystick(RobotMap.WHEEL_ID);
-         joyThrottle = new Joystick(RobotMap.THROTTLE_ID);
-         
-         joyOp = new Joystick(RobotMap.OPERATOR_ID);
-         
-         canTalonFlyWheel = new CANTalon(RobotMap.FLY_WHEEL_CAN_ID);
-        // canTalonTrigger = new CANTalon(26);
-         
-         canTalonIntake = new CANTalon(RobotMap.INTAKE_CAN_ID);
-         
-         canTalonFrontLeft = new CANTalon(RobotMap.FRONT_LEFT_CAN_ID);
-         canTalonFrontRight = new CANTalon(RobotMap.FRONT_RIGHT_CAN_ID);
-         canTalonRearLeft = new CANTalon(RobotMap.REAR_LEFT_CAN_ID);
-         canTalonRearRight = new CANTalon(RobotMap.REAR_RIGHT_CAN_ID);
 
-         canTalonShooterAngle = new CANTalon(RobotMap.SHOOTER_ANGLE_CAN_ID);
-         
-         robotDrive = new RobotDrive(canTalonFrontLeft, canTalonRearLeft, canTalonFrontRight, canTalonRearRight);
-         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-     	 robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
-     	 robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
-     	 
-     	 //shooterLimitTop = new DigitalInput(1);
-     	 //shooterLimitBot = new DigitalInput(4);
-     	 //intakeLimitTop = new DigitalInput(2);
-     	 //intakeLimitBot = new DigitalInput(3);
-     	 opticSensor1 = new DigitalInput(RobotMap.OPTIC_SENSOR_1_DIGITAL_INPUT_PORT);
-     	 opticSensor2 = new DigitalInput(RobotMap.OPTIC_SENSOR_2_DIGITAL_INPUT_PORT);
-     	 
-     	 intakePiston = new DoubleSolenoid(RobotMap.INTAKE_PISTON_EXPAND_CHANNEL, RobotMap.INTAKE_PISTON_RETRACT_CHANNNEL);
-     	 shiftRight = new DoubleSolenoid(RobotMap.PISTON_SHIFT_RIGHT_EXPAND_CHANNEL, RobotMap.PISTON_SHIFT_RIGHT_RETRACT_CHANNEL);
-     	 shiftLeft = new DoubleSolenoid(RobotMap.PISTON_SHIFT_LEFT_EXPAND_CHANNEL, RobotMap.PISTON_SHIFT_LEFT_RETRACT_CHANNEL);
-     	 shooterPiston = new DoubleSolenoid(6,7);
-     	 
-     	 compressor = new Compressor(RobotMap.PCM_CAN_ID);
-     	 
-     	 compressor.setClosedLoopControl(true);
-     	 
-     	 isClose = false;
-     	 
-     	SmartDashboard.putString("Status:", " Working");
-     	 
-    }
-    
-    public void autonomousInit() {
-    	
-    }
+	public static Joystick joyWheel, joyThrottle, joyOp;
+	public static CANTalon canTalonFlyWheel, canTalonIntake, 
+	canTalonFrontLeft, canTalonFrontRight, canTalonRearLeft, canTalonRearRight,
+	canTalonIntakeAngle, canTalonShooterAngle, canTalonGhost;
+	public static RobotDrive robotDrive;
+	public static DigitalInput opticSensor1, opticSensor2;
+	public static CameraServer server;
+	public static PrintWriter system;
+	public static DoubleSolenoid intakePiston, shiftRight, shiftLeft, shooterPiston;
+	public static Compressor compressor;
+	public int isClose;
+	// public static USBCamera camFront = new USBCamera("cam1");
+	// public static USBCamera camRear = new USBCamera("cam2");
+	public void robotInit() {
 
-  
-    public void autonomousPeriodic() {
-    	
-    }
+		server = CameraServer.getInstance();
+		server.setQuality(50);
+		server.startAutomaticCapture("cam1");
+		//     	 camFront.openCamera();
+		//     	 camRear.openCamera();
 
-    public void teleopInit(){
-    	
-    	canTalonShooterAngle.setEncPosition(0);
-    	
-    	
-    }
-    
+		joyWheel = new Joystick(RobotMap.WHEEL_ID);
+		joyThrottle = new Joystick(RobotMap.THROTTLE_ID);
 
-    public void teleopPeriodic() {
-    	
-    	boolean isMinimize = RobotMap.MINIMIZE_BUTTON;
-    	boolean isIntaking = RobotMap.INTAKE_BUTTON;
-    	boolean isReverse = RobotMap.REVERSE_BUTTON;
-    	boolean isFire = RobotMap.FIRE_BUTTON;
-    	boolean isIntakeLower = RobotMap.LOWER_INTAKE_BUTTON;
-    	boolean isIntakeRise = RobotMap.RISE_INTAKE_BUTTON;
-    	boolean stopFlyWheel = RobotMap.STOP_FLYWHEEL_BUTTON;
-    	boolean closeAngle = RobotMap.CLOSE_ANGLE_BUTTON;
-    	boolean farAngle = RobotMap.FAR_ANGLE_BUTTON;
-    	boolean optic = RobotMap.OPTIC_SENSOR_VALUE;
-    	boolean optic2 = RobotMap.OPTIC_SENSOR_VALUE_2;
-    	
-//    	if(joyOp.getRawButton(9)){
-//    		camFront.startCapture();
-//    		camRear.stopCapture();
-//    	}
-//    	else{
-//    		camFront.stopCapture();
-//    		camRear.startCapture();
-//    	}
-    	
-    	//Drive Code
-    	if (isMinimize){
-    		robotDrive.arcadeDrive(joyThrottle.getY()*.6, joyWheel.getX()* .6);
-    	}
-    	else{
-    		robotDrive.arcadeDrive(joyThrottle.getY(), joyWheel.getX());
-    	}
-    	
-    	if(joyOp.getRawButton(8)){
-    		
-    	//controls the state of the intake pistons 
-    	if (isIntakeLower){
-    		intakePiston.set(DoubleSolenoid.Value.kForward);
-    	}
-    	if (isIntakeRise){
-    		intakePiston.set(DoubleSolenoid.Value.kReverse);   		
-    	}
-    
-    	
-    		System.out.println(canTalonFlyWheel.getSpeed());
-    	
-    	    	
-    	//intake
-    	if (isIntaking && !optic && !optic2 ){  //TODO: add the sensor
-    		Intake.spin(.8);
-    	}
-    		else{
-    			Intake.stop();
-    	}
-    	
-    	//reverse intake
-    	if (isReverse){
-    		Intake.spit(.8);
-    	}
-    		else{
-    			Intake.stop();
-    	}
-    	
-    	//Firing
-    	if (isFire){
-    		if(isClose){
-    			Shooter.spinFlyWheel(.7);
-    			Shooter.fire(.8); 
-    		}
-    		else{
-    			//TODO: Camera assisted firing
-    			boolean isDone = Shooter.setPID(0);
-    			if (isDone){
-    				Shooter.setPID(0);
-    				Shooter.fire(.8);
-    			}
-    		}
-    	}
-    		else{
-    			Shooter.stop();
-    			Shooter.stopFlyWheel();
-    	}
-    	
-    	
-    	//Shooter angle control
-    	if (closeAngle){
-    		isClose = true;
-    		boolean done = Shooter.movePID(0);
-    		if (done){
-    			Shooter.stopAngle();
-    		}
-    	}
-    	if(farAngle){
-    		isClose = false;
-    		boolean done1 = Shooter.movePID(0);
-    		if(done1){
-    			Shooter.stopAngle();
-    		}
-    	}
-    	
-    	//Experimental code
-    	
-    	if (joyOp.getRawButton(10) && !optic){
-    		canTalonShooterAngle.set(.5);
-    	}
-    	else {
-    		canTalonShooterAngle.set(0);
-    	}
-    	
-    	if (joyOp.getRawButton(11)){
-    		canTalonFlyWheel.set(.5);
-    	}
-    	if (joyOp.getRawButton(12)){
-    		boolean isdone = Shooter.setPID(70);
-    		if(isdone){
-    			Shooter.stopFlyWheel();
-    		}
-    	}
-    	}
-    	//Experimental code end
-    
-    }
-   
-    public void testPeriodic() {
-        	System.out.println("hello");
-    }
-    
+		joyOp = new Joystick(RobotMap.OPERATOR_ID);
+
+		canTalonFlyWheel = new CANTalon(RobotMap.FLY_WHEEL_CAN_ID);
+		//canTalonTrigger = new CANTalon(26);
+
+		canTalonIntake = new CANTalon(RobotMap.INTAKE_CAN_ID);
+		canTalonGhost = new CANTalon(28);
+
+		canTalonFrontLeft = new CANTalon(RobotMap.FRONT_LEFT_CAN_ID);
+		canTalonFrontRight = new CANTalon(RobotMap.FRONT_RIGHT_CAN_ID);
+		canTalonRearLeft = new CANTalon(RobotMap.REAR_LEFT_CAN_ID);
+		canTalonRearRight = new CANTalon(RobotMap.REAR_RIGHT_CAN_ID);
+
+		canTalonShooterAngle = new CANTalon(RobotMap.SHOOTER_ANGLE_CAN_ID);
+
+		robotDrive = new RobotDrive(canTalonFrontLeft, canTalonRearLeft, canTalonFrontRight, canTalonRearRight);
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+
+		//shooterLimitTop = new DigitalInput(1);
+		//shooterLimitBot = new DigitalInput(4);
+		//intakeLimitTop = new DigitalInput(2);
+		//intakeLimitBot = new DigitalInput(3);
+		opticSensor1 = new DigitalInput(RobotMap.OPTIC_SENSOR_1_DIGITAL_INPUT_PORT);
+		opticSensor2 = new DigitalInput(RobotMap.OPTIC_SENSOR_2_DIGITAL_INPUT_PORT);
+
+		intakePiston = new DoubleSolenoid(RobotMap.INTAKE_PISTON_EXPAND_CHANNEL, RobotMap.INTAKE_PISTON_RETRACT_CHANNNEL);
+		shiftRight = new DoubleSolenoid(RobotMap.PISTON_SHIFT_RIGHT_EXPAND_CHANNEL, RobotMap.PISTON_SHIFT_RIGHT_RETRACT_CHANNEL);
+		shiftLeft = new DoubleSolenoid(RobotMap.PISTON_SHIFT_LEFT_EXPAND_CHANNEL, RobotMap.PISTON_SHIFT_LEFT_RETRACT_CHANNEL);
+		shooterPiston = new DoubleSolenoid(6,7);
+
+		compressor = new Compressor(RobotMap.PCM_CAN_ID);
+
+		compressor.setClosedLoopControl(true);
+
+		isClose = 0;
+
+		SmartDashboard.putString("Status:", " Working");
+
+	}
+
+	public void autonomousInit() {
+
+	}
+
+
+	public void autonomousPeriodic() {
+
+	}
+
+	public void teleopInit(){
+
+		//	canTalonShooterAngle.setEncPosition(0);
+
+
+	}
+
+
+	public void teleopPeriodic() {
+
+		boolean isMinimize = RobotMap.MINIMIZE_BUTTON;
+		boolean isIntaking = RobotMap.INTAKE_BUTTON;
+		boolean isReverse = RobotMap.REVERSE_BUTTON;
+		boolean isFire = RobotMap.FIRE_BUTTON;
+		boolean isIntakeLower = RobotMap.LOWER_INTAKE_BUTTON;
+		boolean isIntakeRise = RobotMap.RISE_INTAKE_BUTTON;
+		boolean stopFlyWheel = RobotMap.STOP_FLYWHEEL_BUTTON;
+		boolean closeAngle = RobotMap.CLOSE_ANGLE_BUTTON;
+		boolean farAngle = RobotMap.FAR_ANGLE_BUTTON;
+		boolean optic = RobotMap.OPTIC_SENSOR_VALUE;
+		boolean optic2 = RobotMap.OPTIC_SENSOR_VALUE_2;
+		boolean isCollapse = joyOp.getRawButton(12);
+
+		//    	if(joyOp.getRawButton(9)){
+		//    		camFront.startCapture();
+		//    		camRear.stopCapture();
+		//    	}
+		//    	else{
+		//    		camFront.stopCapture();
+		//    		camRear.startCapture();
+		//    	}
+
+		//Drive Code
+		if (isMinimize){
+			robotDrive.arcadeDrive(joyThrottle.getY()*.6, joyWheel.getX()* .6);
+		}
+		else{
+			robotDrive.arcadeDrive(joyThrottle.getY(), joyWheel.getX());
+		}
+
+		
+			//controls the state of the intake pistons 
+			if (isIntakeLower){
+				intakePiston.set(DoubleSolenoid.Value.kForward);
+				if (!optic && !optic2){
+					Intake.spin(.8);
+				}
+			}
+			if (isIntakeRise){
+				intakePiston.set(DoubleSolenoid.Value.kReverse);   		
+			}
+
+
+			System.out.println(canTalonFlyWheel.getSpeed());
+		
+
+		//intake
+		if (isIntaking){  //TODO: add the sensor
+			Intake.spin(.8);
+		}
+		else{
+			Intake.stop();
+		}
+
+		//reverse intake
+		if (isReverse){
+			Intake.spit(.8);
+		}
+		else{
+			Intake.stop();
+		}
+
+		//Firing
+		if (isFire){
+			if(isClose == 0){
+				Shooter.spinFlyWheel(.7);
+				Shooter.fire(.8); 
+			}
+			else if (isClose == 1){
+				//TODO: Camera assisted firing
+				boolean isDone = Shooter.setPID(0);
+				if (isDone){
+					Shooter.setPID(0);
+					Shooter.fire(.8);
+				}
+			}
+		}
+		else{
+			Shooter.spinFlyWheel(.7);
+			Shooter.stopFlyWheel();
+		}
+
+
+		//Shooter angle control
+		if (closeAngle){
+			isClose = 1;
+			boolean done = Shooter.movePID(0);
+			if (done){
+				Shooter.stopAngle();
+			}
+		}
+		if(farAngle){
+			isClose = 0;
+			boolean done1 = Shooter.movePID(0);
+			if(done1){
+				Shooter.stopAngle();
+			}
+		}
+		
+		if (isCollapse){
+			intakePiston.set(DoubleSolenoid.Value.kForward);
+			isClose = 2;
+			boolean done2 = Shooter.movePID(0);
+			if(done2){
+				Shooter.stopAngle();
+			}
+		}
+
+		
+	}
+
+	public void testPeriodic() {
+		System.out.println("hello");
+	}
+
 }
--- src/org/usfirst/frc/team668/robot/RobotMap.java
+++ src/org/usfirst/frc/team668/robot/RobotMap.java
@@ -4,11 +4,13 @@ public class RobotMap {
 	
 	// Talon CAN IDs
 	public static final int FLY_WHEEL_CAN_ID = 26;
-	public static final int INTAKE_CAN_ID = 25;
-	public static final int FRONT_LEFT_CAN_ID = 21;
-	public static final int FRONT_RIGHT_CAN_ID = 23;
-	public static final int REAR_LEFT_CAN_ID = 22;
-	public static final int REAR_RIGHT_CAN_ID = 24;
+	public static final int INTAKE_CAN_ID = 27;
+	
+	public static final int FRONT_LEFT_CAN_ID = 24;
+	public static final int FRONT_RIGHT_CAN_ID = 21;
+	public static final int REAR_LEFT_CAN_ID = 23;
+	public static final int REAR_RIGHT_CAN_ID = 22;
+	
 	public static final int SHOOTER_ANGLE_CAN_ID = 25;
 	
 	public static final int OPTIC_SENSOR_1_DIGITAL_INPUT_PORT = 0;
@@ -30,12 +32,12 @@ public class RobotMap {
 	public static final boolean MINIMIZE_BUTTON = Robot.joyThrottle.getRawButton(3);
 	public static final boolean INTAKE_BUTTON = Robot.joyOp.getRawButton(2);
 	public static final boolean REVERSE_BUTTON = Robot.joyOp.getRawButton(3);
-	public static final boolean FIRE_BUTTON = Robot.joyOp.getRawButton(3);
-	public static final boolean LOWER_INTAKE_BUTTON = Robot.joyOp.getRawButton(7);
-	public static final boolean RISE_INTAKE_BUTTON = Robot.joyOp.getRawButton(8);
-	public static final boolean STOP_FLYWHEEL_BUTTON = Robot.joyOp.getRawButton(5);
-	public static final boolean CLOSE_ANGLE_BUTTON = Robot.joyOp.getRawButton(6);
-	public static final boolean FAR_ANGLE_BUTTON = Robot.joyOp.getRawButton(4);
+	public static final boolean FIRE_BUTTON = Robot.joyOp.getRawButton(4);
+	public static final boolean LOWER_INTAKE_BUTTON = Robot.joyOp.getRawButton(5);
+	public static final boolean RISE_INTAKE_BUTTON = Robot.joyOp.getRawButton(6);
+	public static final boolean STOP_FLYWHEEL_BUTTON = Robot.joyOp.getRawButton(7);
+	public static final boolean CLOSE_ANGLE_BUTTON = Robot.joyOp.getRawButton(8);
+	public static final boolean FAR_ANGLE_BUTTON = Robot.joyOp.getRawButton(9);
 	
 	public static final boolean OPTIC_SENSOR_VALUE = Robot.opticSensor1.get();
 	public static final boolean OPTIC_SENSOR_VALUE_2 = Robot.opticSensor2.get();
